fde6551edd5d1a5d7f60f50bb1d01ac4365ca580
Mian Khalid <mkhalid@edx.org>
2018-01-03 17:35:31 +0500

    Move function from activity to fragment

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/MyCoursesListActivity.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/MyCoursesListActivity.java
@@ -13,9 +13,7 @@ import org.edx.mobile.BuildConfig;
 import org.edx.mobile.R;
 import org.edx.mobile.base.BaseSingleFragmentActivity;
 import org.edx.mobile.event.NewVersionAvailableEvent;
-import org.edx.mobile.model.api.EnrolledCoursesResponse;
 import org.edx.mobile.module.analytics.Analytics;
-import org.edx.mobile.module.db.DataCallback;
 import org.edx.mobile.module.notification.NotificationDelegate;
 import org.edx.mobile.module.prefs.PrefManager;
 import org.edx.mobile.util.AppConstants;
@@ -24,7 +22,6 @@ import org.edx.mobile.util.IntentFactory;
 import org.edx.mobile.util.Version;
 
 import java.text.ParseException;
-import java.util.ArrayList;
 
 import de.greenrobot.event.EventBus;
 import roboguice.inject.InjectView;
@@ -112,39 +109,6 @@ public class MyCoursesListActivity extends BaseSingleFragmentActivity {
         super.onResume();
         notificationDelegate.checkAppUpgrade();
     }
-
-    public void updateDatabaseAfterDownload(ArrayList<EnrolledCoursesResponse> list) {
-        if (list != null && list.size() > 0) {
-            //update all videos in the DB as Deactivated
-            environment.getDatabase().updateAllVideosAsDeactivated(dataCallback);
-
-            for (int i = 0; i < list.size(); i++) {
-                //Check if the flag of isIs_active is marked to true,
-                //then activate all videos
-                if (list.get(i).isIs_active()) {
-                    //update all videos for a course fetched in the API as Activated
-                    environment.getDatabase().updateVideosActivatedForCourse(list.get(i).getCourse().getId(),
-                            dataCallback);
-                } else {
-                    list.remove(i);
-                }
-            }
-
-            //Delete all videos which are marked as Deactivated in the database
-            environment.getStorage().deleteAllUnenrolledVideos();
-        }
-    }
-
-    private DataCallback<Integer> dataCallback = new DataCallback<Integer>() {
-        @Override
-        public void onResult(Integer result) {
-        }
-
-        @Override
-        public void onFail(Exception ex) {
-            logger.error(ex);
-        }
-    };
 
     /**
      * Event bus callback for new app version availability event.
--- OpenEdXMobile/src/main/java/org/edx/mobile/view/MyCoursesListFragment.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/MyCoursesListFragment.java
@@ -34,6 +34,7 @@ import org.edx.mobile.loader.CoursesAsyncLoader;
 import org.edx.mobile.logger.Logger;
 import org.edx.mobile.model.api.EnrolledCoursesResponse;
 import org.edx.mobile.module.analytics.Analytics;
+import org.edx.mobile.module.db.DataCallback;
 import org.edx.mobile.module.prefs.LoginPrefs;
 import org.edx.mobile.util.NetworkUtil;
 import org.edx.mobile.view.adapters.MyCoursesAdapter;
@@ -163,7 +164,7 @@ public class MyCoursesListFragment extends BaseFragment
         } else if (result.getResult() != null) {
             ArrayList<EnrolledCoursesResponse> newItems = new ArrayList<EnrolledCoursesResponse>(result.getResult());
 
-            ((MyCoursesListActivity) getActivity()).updateDatabaseAfterDownload(newItems);
+            updateDatabaseAfterDownload(newItems);
 
             if (result.getResult().size() > 0) {
                 adapter.setItems(newItems);
@@ -189,6 +190,39 @@ public class MyCoursesListFragment extends BaseFragment
             onOnline();
         }
     }
+
+    public void updateDatabaseAfterDownload(ArrayList<EnrolledCoursesResponse> list) {
+        if (list != null && list.size() > 0) {
+            //update all videos in the DB as Deactivated
+            environment.getDatabase().updateAllVideosAsDeactivated(dataCallback);
+
+            for (int i = 0; i < list.size(); i++) {
+                //Check if the flag of isIs_active is marked to true,
+                //then activate all videos
+                if (list.get(i).isIs_active()) {
+                    //update all videos for a course fetched in the API as Activated
+                    environment.getDatabase().updateVideosActivatedForCourse(list.get(i).getCourse().getId(),
+                            dataCallback);
+                } else {
+                    list.remove(i);
+                }
+            }
+
+            //Delete all videos which are marked as Deactivated in the database
+            environment.getStorage().deleteAllUnenrolledVideos();
+        }
+    }
+
+    private DataCallback<Integer> dataCallback = new DataCallback<Integer>() {
+        @Override
+        public void onResult(Integer result) {
+        }
+
+        @Override
+        public void onFail(Exception ex) {
+            logger.error(ex);
+        }
+    };
 
     @Override
     public void onLoaderReset(Loader<AsyncTaskResult<List<EnrolledCoursesResponse>>> asyncTaskResultLoader) {
