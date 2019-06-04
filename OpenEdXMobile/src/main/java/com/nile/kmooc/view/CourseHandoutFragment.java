0767749ebfcbd51f2085594986b70b50dcc4d525
cjinw <cjinw@kotech.co.kr>
2019-06-04 16:55:14 +0900

    modify announcement webview error (GL error)

--- OpenEdXMobile/src/main/java/com/nile/kmooc/view/CourseHandoutFragment.java
+++ OpenEdXMobile/src/main/java/com/nile/kmooc/view/CourseHandoutFragment.java
@@ -67,7 +67,7 @@ public class CourseHandoutFragment extends BaseFragment implements RefreshListen
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_handout, container, false);
+        return inflater.inflate(R.layout.fragment_webview_with_paddings, container, false);
     }
 
     @SuppressLint("SetJavaScriptEnabled")
